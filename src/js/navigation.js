6ef79453b935ae2f5c2161d4d4141ec53c556a91
x <exit>
2022-09-05 18:45:22 +0000

    Changes to  menu functionalities

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,6 +1,14 @@
 /* eslint-disable linebreak-style */
 const listMenuNav = document.querySelector('.list-menu');
 const menuToggle = document.querySelector('.menu-toggle');
+const menuItems = document.querySelectorAll('.menu-item > a');
+
+menuItems.forEach((menu) => {
+  menu.addEventListener('click', () => {
+    listMenuNav.setAttribute('data-visible', false);
+    menuToggle.setAttribute('aria-expanded', false);
+  });
+});
 
 menuToggle.addEventListener('click', () => {
   const visibility = listMenuNav.getAttribute('data-visible');
