bdfba8969781839ae037b457589ee05b8e474201
x <exit>
2022-09-05 17:46:38 +0000

    Change menu icon

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,13 +1,14 @@
+/* eslint-disable linebreak-style */
 const listMenuNav = document.querySelector('.list-menu');
-const menuToggle = document.querySelector('.menu-toggle')
+const menuToggle = document.querySelector('.menu-toggle');
 
-menuToggle.addEventListener('click',()=>{
-    const visibility = listMenuNav.getAttribute('data-visible')
-    if(visibility==="false"){
-        listMenuNav.setAttribute('data-visible', true);
-        listMenuNav.setAttribute('aria-expanded', true);
-    }else if(visibility==="true"){
-        listMenuNav.setAttribute('data-visible', false);
-        listMenuNav.setAttribute('aria-expanded', false);
-    }
-})
+menuToggle.addEventListener('click', () => {
+  const visibility = listMenuNav.getAttribute('data-visible');
+  if (visibility === 'false') {
+    listMenuNav.setAttribute('data-visible', true);
+    menuToggle.setAttribute('aria-expanded', true);
+  } else if (visibility === 'true') {
+    listMenuNav.setAttribute('data-visible', false);
+    menuToggle.setAttribute('aria-expanded', false);
+  }
+});
