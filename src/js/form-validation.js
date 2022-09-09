c9017fb7d958998ffc2ee1631f10d800fdf82624
x <exit>
2022-09-09 09:20:01 +0000

    Add local storage feature

--- src/js/form-validation.js
+++ src/js/form-validation.js
@@ -1,22 +1,43 @@
+/* eslint-disable no-else-return */
 /* eslint-disable linebreak-style */
 // eslint-disable-next-line linebreak-style
 /* eslint-disable no-useless-escape */
 /* eslint-disable linebreak-style */
-const mail = document.getElementById('mail');
+const txtname = document.getElementById('name');
+const txtmsg = document.getElementById('msg');
+const txtmail = document.getElementById('mail');
 const filter = /^[a-z]+\@[^\s]+\.[a-z]{2,4}$/;
+
+const data = {
+  name: '',
+  mail: '',
+  msg: '',
+};
+
+window.addEventListener('load', () => {
+  const info = localStorage.getItem('data');
+  const jsonObj = JSON.parse(info);
+  txtname.value = jsonObj.name;
+  txtmail.value = jsonObj.mail;
+  txtmsg.value = jsonObj.msg;
+});
 
 // eslint-disable-next-line consistent-return
 const myform = document.getElementById('contact-form');
 
 myform.addEventListener('submit', (event) => {
-  if (!filter.test(mail.value)) {
-    event.preventDefault();
-    const errMsg = document.getElementById('err-msg');
-    errMsg.innerHTML = 'Your email address is invalid!!';
-    errMsg.classList.add('invalid');
-    // eslint-disable-next-line no-unused-expressions
-    mail.focus;
-    return false;
+  if (filter.test(txtmail.value)) {
+    data.name = txtname.value;
+    data.mail = txtmail.value;
+    data.msg = txtmsg.value;
+    localStorage.setItem('data', JSON.stringify(data));
+    return true;
   }
-  return true;
+  event.preventDefault();
+  const errMsg = document.getElementById('err-msg');
+  errMsg.innerHTML = 'Your email address is invalid!!';
+  errMsg.classList.add('invalid');
+  // eslint-disable-next-line no-unused-expressions
+  txtmail.focus;
+  return false;
 });
